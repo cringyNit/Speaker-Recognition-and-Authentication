5a12ea02355460ce2707a4dd3a9848b12e078559
Deepak16031 <deepak16031@iiitd.ac.in>
2020-04-23 17:55:08 +0530

    SRUDeepak

--- GMMTesting.py
+++ GMMTesting.py
@@ -13,4 +13,4 @@ def testSingleaudio(testpath) :
         tmp = Models.retrieveModels('GMMModels/' + model)
         print tmp.score(mfcc_features)
         
-testSingleaudio('cat1.wav')
+testSingleaudio('cat2.wav')
