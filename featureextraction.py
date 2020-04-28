5da99e055fbf273ec819908a7045c0f526188353
Deepak16031 <deepak16031@iiitd.ac.in>
2020-04-28 17:25:05 +0530

    uu

--- featureextraction.py
+++ featureextraction.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.io.wavfile import read as rd
 from sklearn import preprocessing
 import python_speech_features as mfcc
 
@@ -7,30 +8,17 @@ def calculate_delta(array):
 
     rows,cols = array.shape
     deltas = np.zeros((rows,20))
-    N = 2
     for i in range(rows):
-        index = []
-        j = 1
-        while j <= N:
-            if i-j < 0:
-              first =0
-            else:
-              first = i-j
-            if i+j > rows-1:
-                second = rows-1
-            else:
-                second = i+j 
-            index.append((second,first))
-            j+=1
-        deltas[i] = ( array[index[0][0]]-array[index[0][1]] + (2 * (array[index[1][0]]-array[index[1][1]])) ) / 10
+        deltas[i] = ( array[i+1 if i+1 < rows else rows-1]-array[i-1 if i-1>0 else 0] + (2 * (array[i+2 if i+2<rows else rows-1]-array[i-2 if i-2>=0 else 0])) ) / 10
     return deltas
 
 def extract_features(audio,rate):
     """extract 20 dim mfcc features from an audio, performs CMS and combines 
     delta to make it 40 dim feature vector"""    
     
-    mfcc_feature = mfcc.mfcc(audio,rate, 0.025, 0.01,20,nfft = 1200, appendEnergy = True)    
+    mfcc_feature = mfcc.mfcc(audio,rate, 0.025, 0.01,20,nfft = 1200, appendEnergy = True)   
     mfcc_feature = preprocessing.scale(mfcc_feature)
     delta = calculate_delta(mfcc_feature)
     combined = np.hstack((mfcc_feature,delta)) 
     return combined
+
